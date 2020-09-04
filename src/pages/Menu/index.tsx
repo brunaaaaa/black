5dbbf0606903c2327c1934ee2f51f88d80be152c
BetaOrtiz <emaildabetaortiz@gmail.com>
2020-09-04 18:43:29 -0300

    connected to firebase

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './styles.css'
+import './styles.css';
 
 import img1 from '../../assets/imgs/menu-fatura.svg';
 import img2 from '../../assets/imgs/menu-limite.svg';
@@ -12,7 +12,7 @@ function Menu() {
     <div id="menu">
       <Link to="/fatura" className="menu-link">
         <div className="menu-card">
-          <img src={img1} alt="Fatura atual"/>
+          <img src={img1} alt="Fatura atual" />
           <h2>Fatura atual</h2>
           <p>Come on, flip the pickle, Morty. You're not gonna regret it.</p>
         </div>
@@ -20,7 +20,7 @@ function Menu() {
 
       <Link to="/limite" className="menu-link">
         <div className="menu-card">
-          <img src={img2} alt="Limite de crédito"/>
+          <img src={img2} alt="Limite de crédito" />
           <h2>Limite de crédito</h2>
           <p>Come on, flip the pickle, Morty. You're not gonna regret it.</p>
         </div>
@@ -28,13 +28,21 @@ function Menu() {
 
       <Link to="/sonho" className="menu-link">
         <div className="menu-card">
-          <img src={img3} alt="Cofrinho dos sonhos"/>
+          <img src={img3} alt="Cofrinho dos sonhos" />
           <h2>Cofrinho dos sonhos</h2>
           <p>Come on, flip the pickle, Morty. You're not gonna regret it.</p>
         </div>
       </Link>
+
+      <Link to="/pagamentos" className="menu-link">
+        <div className="menu-card">
+          <img src={img3} alt="Pagamento de contas" />
+          <h2>Pagamentos e transferências</h2>
+          <p>Come on, flip the pickle, Morty. You're not gonna regret it.</p>
+        </div>
+      </Link>
     </div>
-  )
+  );
 }
 
 export default Menu;
