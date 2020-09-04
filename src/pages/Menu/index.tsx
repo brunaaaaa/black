64aea4c73b868e181bdd85058f407b1a66d15821
Bruna Nolasco <brunamnolasco@gmail.com>
2020-09-03 21:19:16 -0300

    pagina de fatura

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -5,27 +5,34 @@ import './styles.css'
 import img1 from '../../assets/imgs/menu-fatura.svg';
 import img2 from '../../assets/imgs/menu-limite.svg';
 import img3 from '../../assets/imgs/menu-sonho.svg';
+import { Link } from 'react-router-dom';
 
 function Menu() {
   return (
     <div id="menu">
-      <div className="menu-card">
-        <img src={img1} alt="Fatura atual"/>
-        <h2>Fatura atual</h2>
-        <p>Come on, flip the pickle, Morty. You're not gonna regret it.</p>
-      </div>
+      <Link to="/fatura" className="menu-link">
+        <div className="menu-card">
+          <img src={img1} alt="Fatura atual"/>
+          <h2>Fatura atual</h2>
+          <p>Come on, flip the pickle, Morty. You're not gonna regret it.</p>
+        </div>
+      </Link>
 
-      <div className="menu-card">
-        <img src={img2} alt="Limite de crédito"/>
-        <h2>Limite de crédito</h2>
-        <p>Come on, flip the pickle, Morty. You're not gonna regret it.</p>
-      </div>
+      <Link to="/limite" className="menu-link">
+        <div className="menu-card">
+          <img src={img2} alt="Limite de crédito"/>
+          <h2>Limite de crédito</h2>
+          <p>Come on, flip the pickle, Morty. You're not gonna regret it.</p>
+        </div>
+      </Link>
 
-      <div className="menu-card">
-        <img src={img3} alt="Cofrinho dos sonhos"/>
-        <h2>Cofrinho dos sonhos</h2>
-        <p>Come on, flip the pickle, Morty. You're not gonna regret it.</p>
-      </div>
+      <Link to="/sonho" className="menu-link">
+        <div className="menu-card">
+          <img src={img3} alt="Cofrinho dos sonhos"/>
+          <h2>Cofrinho dos sonhos</h2>
+          <p>Come on, flip the pickle, Morty. You're not gonna regret it.</p>
+        </div>
+      </Link>
     </div>
   )
 }
