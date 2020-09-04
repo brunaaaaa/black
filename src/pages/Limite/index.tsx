d494a392a3a5d41e29e92589799a02c0395be2e8
Bruna Nolasco <brunamnolasco@gmail.com>
2020-09-03 22:25:47 -0300

    telas iniciais finalizadas

--- src/pages/Limite/index.tsx
+++ src/pages/Limite/index.tsx
@@ -2,10 +2,25 @@ import React from 'react';
 
 import './styles.css'
 
+import limiteImg from '../../assets/imgs/limite.svg';
+
 function Limite() {
   return (
-    <div>
-      Limite
+    <div id="limite">
+      <img src={limiteImg} alt="Limite"/>
+      <h2>Limite</h2>
+
+      <div id="limite-info">
+        <h2>Seu limite de crédito atual</h2>
+        <h2>R$ 600,00</h2>
+        <h3>Aumente seus limites seguindo essas dicas</h3>
+        <ul>
+          <li>Pague a fatura do seu cartão antes do dia de vencimento</li>
+          <li>Coloque dinheiro no cofrinho dos sonhos</li>
+          <li>Did you get those seeds all the way up your butt?</li>
+          <li>It's called carpe diem Morty. Look it up</li>
+        </ul>
+      </div>
     </div>
   )
 }
--- src/pages/Sonho/index.tsx
+++ src/pages/Sonho/index.tsx
@@ -2,10 +2,43 @@ import React from 'react';
 
 import './styles.css'
 
+import sonhoImg from '../../assets/imgs/sonho.svg';
+import moto from '../../assets/imgs/sonho-moto.png';
+
 function Sonho() {
   return (
-    <div>
-      Sonho
+    <div id="sonho">
+      <img src={sonhoImg} alt="Cofrinho dos sonhos"/>
+      <h2>Cofrinho dos sonhos</h2>
+
+      <div id="sonho-info">
+        <div className="grid">
+          <h3>Saldo Total:</h3>
+          <h3>R$ 78,45</h3>
+        </div>
+        <div className="grid">
+          <h3>Meta:</h3>
+          <h3>R$ 7.500,00</h3>
+        </div>
+
+        <h4>Você está <span className="destaque">1.04%</span> mais perto de conquistar sua <span className="destaque">moto</span>!</h4>
+        
+        <h4>Sonhos / Objetivos cadastrados</h4>
+        
+        <div className="each-dream">
+          <img src={moto} alt="Foto do sonho"/>
+          <div className="dream-info">
+            <p><strong>Título:</strong> Moto</p>
+            <p><strong>Valor:</strong> R$ 7.500,00</p>
+            <p><strong>Descrição:</strong> Yamaha Neo 125</p>
+          </div>
+        </div>
+
+        <div className="add-dream">
+          <span className="iconify add" data-icon="ant-design:plus-circle-outlined" data-inline="false"></span>
+          <span>Cadastrar novo Sonho / Objetivo</span>
+        </div>
+      </div>
     </div>
   )
 }
